edc4a3613e2bf471edc868d81a5e5a997f465c46
MjavadF <javad.me1376@gmail.com>
2021-12-28 20:01:54 +0330

    implement first view

--- rules/views.py
+++ rules/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def welcome(request):
+    return render(request=request, template_name='rules/rules.html')
