6e1790dd5571206e20d8983d2a24ce468179b091
MjavadF <javad.me1376@gmail.com>
2021-12-29 14:17:24 +0330

    remove secret_key from settings.py

--- ma_bugim/settings.py
+++ ma_bugim/settings.py
@@ -11,16 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECTER_KEY = config('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_w^ow0isno1h@67yffllc*jkc@ljqtduzvhpy^d9!zidokr-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
